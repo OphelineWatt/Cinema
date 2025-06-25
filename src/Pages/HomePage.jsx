701a9649c6866d01bdad3b86d7aa3c56150689ed
Ophéline Wattier <opheliiinee@gmail.com>
2025-06-25 11:20:59 +0200

    ajout search barre

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,14 +2,17 @@ import { useEffect, useState } from 'react';
 import MovieCard from '../Components/MovieCard';
 import axios from 'axios';
 import { Button, Col, Form, Row } from 'react-bootstrap';
+import '../styles/homePage.css'
 
 const HomePage = () => {
     const [movies, setMovies] = useState([]);
     const [page, setPage] = useState(1);
     const [totalPages, setTotalPages] = useState();
+    const [search, setSearch] = useState("");
 
     const fetchMovies = async () => {
         try {
+            if (search ==="") {
             const response = await axios.get(`https://api.themoviedb.org/3/discover/movie?language=fr-FR&page=${page}&include_adult=false`, {
                 headers: {
                     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmY2JlZDMzZDIwNjFjNWUzYzdkODRiMzRjODYzY2M1ZiIsIm5iZiI6MTczNzk4NTMzNy42NjksInN1YiI6IjY3OTc4ZDM5M2Y3ZmNmNjdkMDhmNjFlMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Lg2MGOX1wHrfnwnx90PsNKRLWEv80G8K6RW4qCyABoo'
@@ -18,7 +21,16 @@ const HomePage = () => {
             setMovies(response.data.results);
             console.log(response.data);
             setTotalPages(response.data.total_pages);
-
+        } else {
+            const response = await axios.get(`https://api.themoviedb.org/3/search/movie?language=fr-FR&query=${search}&page=${page}&include_adult=false`, {
+                headers: {
+                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmY2JlZDMzZDIwNjFjNWUzYzdkODRiMzRjODYzY2M1ZiIsIm5iZiI6MTczNzk4NTMzNy42NjksInN1YiI6IjY3OTc4ZDM5M2Y3ZmNmNjdkMDhmNjFlMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Lg2MGOX1wHrfnwnx90PsNKRLWEv80G8K6RW4qCyABoo'
+                } 
+            });
+            setMovies(response.data.results);
+            console.log(response.data);
+            setTotalPages(response.data.total_pages);
+        }
 
         } catch (error) {
             console.error("Error fetching movies:", error);
@@ -27,29 +39,27 @@ const HomePage = () => {
 
     useEffect(() => {
         fetchMovies();
-    }, [page])
+    }, [page, search]);
+
+    const handleSearchChange = (e) => {
+        setSearch(e.target.value);
+        setPage(1);
+    }
 
     return <div className='d-flex flex-column align-items-center justify-content-center'>
         <h1>HomePage</h1>
 
-        <Form >
-        <Row>
-          <Col xs="auto">
-            <Form.Control
-              type="text"
-              placeholder="Search"
-              className=" mr-sm-2"
-            />
-          </Col>
-          <Col xs="auto">
-            <Button type="submit">Submit</Button>
-          </Col>
-        </Row>
-      </Form>
+    <input
+        type='texte'
+        className='barre form-control col-8'
+        placeholder='Rechercher un film'
+        value={search}
+        onChange={handleSearchChange}
+    />
 
         <div className='d-flex justify-content-between align-items-center col-8'>
             <button className='btn btn-outline-primary' onClick={() => setPage(page - 1)} disabled={page === 1}>◀ Précédent​</button>
-            <span>Page {page} sur {totalPages}</span>
+            <span className='pagination'>Page {page} sur {totalPages}</span>
             <button className='btn btn-outline-primary' onClick={() => setPage(page + 1)} disabled={page === totalPages}>Suivant ▶​</button>
         </div>
 
@@ -60,7 +70,7 @@ const HomePage = () => {
         </div>
         <div className='d-flex justify-content-between align-items-center col-8'>
             <button className='btn btn-outline-primary' onClick={() => setPage(page - 1)} disabled={page === 1}>◀ Précédent​</button>
-            <span>Page {page} sur {totalPages}</span>
+            <span className='pagination'>Page {page} sur {totalPages}</span>
             <button className='btn btn-outline-primary' onClick={() => setPage(page + 1)} disabled={page === totalPages}>Suivant ▶​</button>
         </div>
     </div>;
--- src/Pages/PeoplesPage.jsx
+++ src/Pages/PeoplesPage.jsx
@@ -6,9 +6,13 @@ import { Button, Col, Form, Row } from 'react-bootstrap';
 const PeoplesPage = () => {
 
 const[peoples, SetPeoples] = useState([]);
+const [search, setSearch] = useState("");
+const [page, setPage] = useState(1);
+const [totalPages, setTotalPages] = useState();
 
     const fetchPeoples= async () => {
-        try {
+      try {
+      if (search ===("")) {
             const response = await axios.get("https://api.themoviedb.org/3/person/popular", {
                 headers: {
                     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NDczNTRkNzZiZTM2NTcxODY4NDcyZGZhZWUyN2Q4NyIsIm5iZiI6MTY0Njk4ODUwNS4xMjgsInN1YiI6IjYyMmIwY2Q5ZDY4MTliMDAxYjVhMjUwNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Yag79kgVwxdazfAOqQIOXnt1G7xh8MUbSf5EARMpv9Q'
@@ -16,9 +20,18 @@ const[peoples, SetPeoples] = useState([]);
             });
 
             console.log(response);
-            
-
             SetPeoples(response.data.results);
+          } else {
+                       const response = await axios.get(`https://api.themoviedb.org/3/search/person?language=fr-FR&query=${search}&page=${page}&include_adult=false`, {
+                headers: {
+                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NDczNTRkNzZiZTM2NTcxODY4NDcyZGZhZWUyN2Q4NyIsIm5iZiI6MTY0Njk4ODUwNS4xMjgsInN1YiI6IjYyMmIwY2Q5ZDY4MTliMDAxYjVhMjUwNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Yag79kgVwxdazfAOqQIOXnt1G7xh8MUbSf5EARMpv9Q'
+                }
+            });
+
+            console.log(response);
+            SetPeoples(response.data.results);
+
+          }
 
           
             
@@ -30,29 +43,37 @@ const[peoples, SetPeoples] = useState([]);
    
     useEffect(() => {
     fetchPeoples();
-    }, [])
+    }, [search])
+
+        const handleSearchChange = (e) => {
+        setSearch(e.target.value);
+        setPage(1);
+    }
     
     return <div className="d-flex flex-column align-items-center justify-content-center">
     <h1>Peoples Page</h1>
-    <Form >
-        <Row>
-          <Col xs="auto">
-            <Form.Control
-              type="text"
-              placeholder="Search"
-              className=" mr-sm-2"
-            />
-          </Col>
-          <Col xs="auto">
-            <Button type="submit">Submit</Button>
-          </Col>
-        </Row>
-      </Form>
+        <input
+        type='texte'
+        className='barre form-control col-8'
+        placeholder='Rechercher un acteur'
+        value={search}
+        onChange={handleSearchChange}
+    />
+            <div className='d-flex justify-content-between align-items-center col-8'>
+            <button className='btn btn-outline-primary' onClick={() => setPage(page - 1)} disabled={page === 1}>◀ Précédent​</button>
+            <span className='pagination'>Page {page} sur {totalPages}</span>
+            <button className='btn btn-outline-primary' onClick={() => setPage(page + 1)} disabled={page === totalPages}>Suivant ▶​</button>
+        </div>
     <div className="d-flex flex-wrap justify-content-center gap-5 col-8">
     {peoples.map((people) => {
         return < PeopleCard key={people.id} people={people}/>
     })}
     </div>
+            <div className='d-flex justify-content-between align-items-center col-8'>
+            <button className='btn btn-outline-primary' onClick={() => setPage(page - 1)} disabled={page === 1}>◀ Précédent​</button>
+            <span className='pagination'>Page {page} sur {totalPages}</span>
+            <button className='btn btn-outline-primary' onClick={() => setPage(page + 1)} disabled={page === totalPages}>Suivant ▶​</button>
+        </div>
     </div>;
 }
  
